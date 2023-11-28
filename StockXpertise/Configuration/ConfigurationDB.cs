2047325220054098cf2a843770e3a70b4db1b054
Lucas DIAS <dias@et.intechinfo.fr>
2023-11-28 10:30:05 +0100

    Ajout de la connexion a la base de donnée

---
+++ StockXpertise/Configuration/ConfigurationDB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["MyDbConnection"].ConnectionString;
+
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+
+                // Perform database operations here
+
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
