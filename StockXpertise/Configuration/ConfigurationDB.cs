62718832c0564ef86d700d4793c1225c6d755347
DING Celivé <ding@et.intechinfo.fr>
2023-12-04 15:38:37 +0000

    Synchronisation de la branche celive

--- StockXpertise/Configuration/ConfigurationDB.cs
+++ StockXpertise/Configuration/ConfigurationDB.cs
@@ -27,6 +27,8 @@ namespace StockXpertise
 
                     string scriptFilePath = "./Configuration/construct.MySql";
                     ExecuteSqlScript(connection, scriptFilePath);
+                    string insertFilePath = "./Configuration/insert.MySql";
+                    ExecuteSqlScript(connection, insertFilePath);
                 }
             }
             catch (Exception ex)
@@ -43,6 +45,49 @@ namespace StockXpertise
             Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configFileMap, ConfigurationUserLevel.None);
 
             return config.ConnectionStrings.ConnectionStrings["MyDbConnection"]?.ConnectionString;
+        }
+
+        public static void ExecuteQuery(string query)
+        {
+            try
+            {
+                string configFilePath = "./Configuration/config.xml";
+                string connectionString = GetConnectionString(configFilePath);
+
+                if (string.IsNullOrEmpty(connectionString))
+                {
+                    Console.WriteLine("Connection string is null or empty.");
+                    return;
+                }
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection opened successfully.");
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            // Afficher les résultats dans la console
+                            while (reader.Read())
+                            {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    Console.Write($"{reader.GetName(i)}: {reader.GetValue(i)}\t");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("SQL query executed successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in ExecuteQuery: {ex.Message}");
+            }
         }
 
         private static void ExecuteSqlScript(MySqlConnection connection, string scriptFilePath)
