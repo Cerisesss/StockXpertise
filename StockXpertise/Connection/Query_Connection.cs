cd9638b68b65eb744c90d08292ca163cce113f71
DING Celivé <ding@et.intechinfo.fr>
2024-01-12 00:02:51 +0000

    hashache du mot de passe

--- StockXpertise/Connection/Query_Connection.cs
+++ StockXpertise/Connection/Query_Connection.cs
@@ -1,7 +1,10 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,28 +22,47 @@ namespace StockXpertise.Connection
             this.password = password;
         }
 
+        public MySqlConnection ConnectionDB()
+        {
+            // Connexion à la database
+            string configFilePath = "./Configuration/config.xml";
+            string connectionString = ConfigurationDB.GetConnectionString(configFilePath);
+
+            MySqlConnection ConnectionDB = new MySqlConnection(connectionString);
+            ConnectionDB.Open();
+
+            return ConnectionDB;
+        }
+
         public MySqlDataReader Select_Connection()
         {
             MySqlDataReader reader;
 
+            string passwordDB = GetPassword(); //recupere le hash du mdp dans la db
+
+            if (passwordDB == null)
+            {
+                return null;
+            }
+
+            bool isPasswordCorrect = BCrypt.Net.BCrypt.Verify(password, passwordDB);
+
+            if (!isPasswordCorrect)
+            {
+                return null;
+            }
+
             try
             {
                 // Requête SQL paramétrée
-                string query = "SELECT * FROM employes WHERE mail = @Mail AND mot_de_passe = @Password ;";
-
-                // Connexion à la database
-                string configFilePath = "./Configuration/config.xml";
-                string connectionString = ConfigurationDB.GetConnectionString(configFilePath);
-
-                MySqlConnection ConnectionDB = new MySqlConnection(connectionString);
-                ConnectionDB.Open();
+                string query = "SELECT * FROM employes WHERE mail = @Mail ;";
 
                 // Crée une commande SQL avec la requête et la connexion
-                MySqlCommand commande = new MySqlCommand(query, ConnectionDB);
+                MySqlCommand commande = new MySqlCommand(query, ConnectionDB());
 
                 // Ajoute les paramètres à la commande pour eviter les injections SQL
                 commande.Parameters.AddWithValue("@Mail", mail);
-                commande.Parameters.AddWithValue("@Password", password);
+                commande.Parameters.AddWithValue("@Password", isPasswordCorrect);
 
                 // Exécute la commande
                 return reader = commande.ExecuteReader();
@@ -49,6 +71,36 @@ namespace StockXpertise.Connection
             {
                 MessageBox.Show($"Error in ConnectionDB: {ex.Message}");
 
+                return null;
+            }
+        }
+
+        public string GetPassword()
+        {
+            MySqlDataReader reader;
+            string mdp = null;
+
+            try
+            {
+                string query = "SELECT * FROM employes WHERE mail = @Mail;";
+
+                MySqlCommand commande = new MySqlCommand(query, ConnectionDB());
+                commande.Parameters.AddWithValue("@Mail", mail);
+
+                reader = commande.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        mdp = reader["mot_de_passe"].ToString();
+                    }
+                }
+                return mdp;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error in ConnectionDB: {ex.Message}");
                 return null;
             }
         }
